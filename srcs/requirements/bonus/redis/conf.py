8e1b05413437fefd5e26203ae3f28c7939736d6a
oumiiii <oaarabe2307@gmail.com>
2024-07-26 04:06:02 +0100

    fix : redis config

--- srcs/requirements/bonus/redis/conf.py
+++ srcs/requirements/bonus/redis/conf.py
@@ -2,17 +2,15 @@ import subprocess
 
 # Define the commands as lists of arguments
 commands = [
-    ['sed', '-i', 's|# bind 127.0.0.1|bind 127.0.0.1|g', '/etc/redis/redis.conf'],
     ['sed', '-i', 's|# maxmemory <bytes>|maxmemory 20mb|g', '/etc/redis/redis.conf']
 ]
 
 # Execute each command
-for command in commands:
-    subprocess.run(command)
+# for command in commands:
+# subprocess.run(['sed', '-i', 's|# bind 127.0.0.1|bind 127.0.0.1|g', '/etc/redis/redis.conf'])
+subprocess.run(['sed', '-i', 's|# maxmemory <bytes>|maxmemory 20mb|g', '/etc/redis/redis.conf'])
 
 line = 'maxmemory-policy allkeys-lru'
 with open('/etc/redis/redis.conf', 'a') as file:
             file.write(line + '\n')
-subprocess.run(["redis-server", "/etc/redis/redis.conf"])
-while True:
-    pass
+subprocess.run(["redis-server", "--protected-mode", "no"])
--- srcs/requirements/wordpress/script.py
+++ srcs/requirements/wordpress/script.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from  os import system
 import os
 import subprocess
 import time
@@ -10,24 +11,9 @@ subprocess.run(['sed', '-i', 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|
 
 # Check if wp-config.php exists
 # if os.path.isfile("wp-config-sample.php"):
-subprocess.run(['mv',  '/var/www/html/wp-config-sample.php', '/var/www/html/wp-config.php'])
+# subprocess.run(['mv',  '/var/www/html/wp-config-sample.php', '/var/www/html/wp-config.php'])
+# subprocess.run(['cp', '/var/www/html/wp-config.php', '/var/www/html/wp-config.php.bak'])
 
-with open('/var/www/html/wp-config.php', 'r') as file:
-    config_lines = file.readlines()
-
-for i, line in enumerate(config_lines):
-    if line.startswith("define( 'DB_NAME'"):
-        config_lines[i] = (f"define( 'DB_NAME', '{os.getenv('MYSQL_DATABASE_NAME')}');")
-    elif line.startswith("define( 'DB_USER'"):
-        config_lines[i] = (f"define( 'DB_USER', '{os.getenv('MYSQL_USER')}');")
-    elif line.startswith("define( 'DB_PASSWORD'"):
-        config_lines[i] = (f"define( 'DB_PASSWORD', '{os.getenv('MYSQL_PASSWORD')}');")
-    elif line.startswith("define( 'DB_HOST'"):
-        config_lines[i] = (f"define( 'DB_HOST', 'mariadb');")
-
-# Write modified content back to wp-config.php
-with open('/var/www/html/wp-config.php', 'w') as file:
-    file.writelines(config_lines)
 
     # Install WordPress with administrative credentials
 subprocess.run(['wp', 'core', 'install', '--url=' + os.getenv('WP_URL'), '--title=Inception',
@@ -37,6 +23,8 @@ subprocess.run(['wp', 'user', 'create', os.getenv('WP_USER'), os.getenv('WP_USER
                 '--user_pass=' + os.getenv('WP_USER_PSWD'), '--path=/var/www/html', '--allow-root'])
 #plugins redis
 subprocess.run(['wp', 'plugin', 'install', 'redis-cache', '--activate', '--path=/var/www/html', '--allow-root'])
+subprocess.run(['wp', 'plugin', 'update', '--all', '--path=/var/www/html', '--allow-root'])
+subprocess.run(['wp', 'redis', 'enable',  '--path=/var/www/html', '--allow-root'])
 
 # Set ownership of WordPress directory
 subprocess.run(['chown', '-R', 'www-data:www-data', '/var/www/html'])
