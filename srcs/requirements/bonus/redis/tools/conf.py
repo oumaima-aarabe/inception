295ceb8fa0d662b722a3d6055a9ace4323d1dc6c
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2024-08-03 11:27:21 +0100

    finish inception

--- srcs/requirements/bonus/redis/tools/conf.py
+++ srcs/requirements/bonus/redis/tools/conf.py
@@ -1,13 +1,5 @@
 import subprocess
 
-# Define the commands as lists of arguments
-commands = [
-    ['sed', '-i', 's|# maxmemory <bytes>|maxmemory 20mb|g', '/etc/redis/redis.conf']
-]
-
-# Execute each command
-# for command in commands:
-# subprocess.run(['sed', '-i', 's|# bind 127.0.0.1|bind 127.0.0.1|g', '/etc/redis/redis.conf'])
 subprocess.run(['sed', '-i', 's|# maxmemory <bytes>|maxmemory 20mb|g', '/etc/redis/redis.conf'])
 
 line = 'maxmemory-policy allkeys-lru'
